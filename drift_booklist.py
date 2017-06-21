41d7c8cdffcc047e5250661900a2fcae7e04d0e5
dyinnz <dyinnz@gmail.com>
2017-06-21 17:18:58 +0800

    login ok

--- drift_booklist.py
+++ drift_booklist.py
@@ -1,7 +1,45 @@
+import flask
 from flask import Flask
 from flask import request
 
+import flask_login
+
 app = Flask(__name__)
+app.secret_key = b'\x12\x89C\xda\xab\xcbD\xd4\x91@\x9b\xde\xbf?Y\x13\xe8Y\xcf\xbc\xaa\x9c"\x93'
+
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+users = {'xlm': {'password': '1234'}}
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def uesr_loader(username):
+    if username not in users:
+        return
+    user = User()
+    user.id = username
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    if username not in user:
+        return
+    user = User()
+    user.id = username
+
+    user.is_authenticated = (
+            request.form['password'] == users[username]['password']
+            )
+
+    return user
 
 
 @app.route('/')
@@ -9,22 +47,35 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login_or_register():
-    return 'login or register'
+    if request.method == 'GET':
+        return '''
+        <form action='login' method='POST'>
+        <input type='text' name='username' id='username' placeholder='username'></input>
+        <input type='password' name='password' id='password' placeholder='password'></input>
+        <input type='submit' name='submit'></input>
+        </form>
+        '''
+
+    username = flask.request.form['username']
+    if flask.request.form['password'] == users[username]['password']:
+        user = User()
+        user.id = username
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('start'))
+
+    return 'Login Error!'
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        return 'post'
-    else:
-        return 'get'
-
+    return 'register'
 
 @app.route('/start')
-def newbie_start():
-    return 'start'
+@flask_login.login_required
+def start():
+    return 'start as: ' + flask_login.current_user.id
 
 
 @app.route('/explore')
