860b6d0db7fcd04daaf1b88902519f925cad84f8
xpriny <xlm104600@gmail.com>
2017-06-23 19:23:37 +0800

    change mine_page

--- drift_app/mine_page.py
+++ drift_app/mine_page.py
@@ -1,60 +1,58 @@
 import flask
+import json
 import logging
 from flask import Blueprint,jsonify,render_template
 from flask import request
-from drift_app.db_interface.db_book import get_book
+import drift_app.db_interface.db_book as db_book
+import drift_app.db_interface.db_user_remark as db_user_remark
 import flask_login
 
 mine_bp = Blueprint('mine_bp',__name__)
-'''
-with flask.current_app.test_request_context('/booklistdetail', method='POST'):
-    assert request.path == '/booklistdetail'
-    assert request.method == 'POST'
-'''
 
 
 @mine_bp.route('/test')
 def test():
-    return flask.current_app.send_static_file('main_test.html')
-    #return flask.render_template('mine_test.html')
-    #return flask.redirect(flask.url_for('.mine'))
-
-@mine_bp.route('/test/add')
-def add_numbers():
-    """Add two numbers server side, ridiculous but well..."""
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
+    return flask.current_app.send_static_file('mine.html')
+'''
+@mine_bp.route('/test/data')
+def testclick():
     logging.debug('add succes')
-    return jsonify(result = a + b)
-    #return 'add'
-
+    return 'add'
+'''
 @mine_bp.route('/mine')
 def mine():
     if flask_login.current_user.is_anonymous:
         return flask.redirect(flask.url_for('login_bp.login'))
     else:
-        account = flask_login.current_user.id
+        return flask.current_app.send_static_file('mine.html')
 
-    booklistinfo={'booklistname':'name','createuser':'user','like':1000,'remark':1000,'label':'babel','introduce':'introduce'}
-    bookinfo={'book1':'book1'}
-    booklist=[bookinfo]
-    booklistinfo['booklist']=booklist
-
-    mybooklist={'booklist1':'booklist1'}
-    likebooklist={'booklist2':'booklist2'}
-    jsondata=[mybooklist,likebooklist,booklistinfo,account]
+@mine_bp.route('/get_mydata')
+def get_mydata():
+    user_id=flask_login.current_user.id
+    jsondata={
+        'booklist_id':'id',
+        'booklist_cover':'cover',
+        'booklist_name':'name',
+        'create_user':'username',
+        'book_number':1000,
+        'follower_number':1000,
+        #some other data
+        'booklist':['book1','book2','book3'],
+        'my_booklist':['list1','list2','list3'],
+        'follower_booklist':['list1','list2','list3']
+    }
     '''
-    get mine data
+    get my data
     '''
     return   jsonify(jsondata)
 
-@mine_bp.route('/newbooklist',methods=['POST','GET'])
-def newbooklist():
+@mine_bp.route('/new_booklist',methods=['POST','GET'])
+def new_booklist():
     if request.method=='POST':
-        return 'addbooklist'
-    return  'GET'
+        return 'new_booklist'
+    return  'need post request'
 
-@mine_bp.route('/booklistdetail',methods=['POST','GET'])
+@mine_bp.route('/booklist_detail',methods=['POST','GET'])
 def booklistdetail():
     if request.method=='POST':
         booklistname=request.form.get('booklistname')
@@ -71,58 +69,89 @@ def booklistdetail():
     else:
         return 'need post request'
 
-@mine_bp.route('/bookdetail',methods=['POST','GET'])
+@mine_bp.route('/book_detail',methods=['POST','GET'])
 def bookdetail():
     if request.method!='POST':
         return 'need POST'
     else:
-        #bookid=request.form.get('bookid')
-        #return get_book(bookid)
-        return 'bookdetail'
+        return 'book_detail'
 
 
-@mine_bp.route('/addbooklistremark',methods=['POST','GET'])
-def addbooklist():
-    if request.method!='POST':
-        return 'need POST'
-    else:
-        booklistname=request.form.get('booklistname');
-        if flask_login.current_user.is_anonymous:
-            return flask.redirect(flask.url_for('login_bp.login'))
-        else:
-            account = flask_login.current_user.id
-        '''
-        addremark
-        '''
-        return jsonify({'OK',True})
-
-@mine_bp.route('/addbookremark',methods=['POST','GET'])
-def addbookremark():
+@mine_bp.route('/add_booklist_remark',methods=['POST','GET'])
+def add_booklist_remark():
     if request.method!='POST':
         return 'need POST'
     else:
         if flask_login.current_user.is_anonymous:
             return flask.redirect(flask.url_for('login_bp.login'))
         else:
-            account = flask_login.current_user.id
+            user_id = flask_login.current_user.id
 
-        bookname=request.form.get('bookname')
-        '''
-        addremark
-        '''
-        return jsonify({'OK',True})
+    data=json.loads(request.get_json())
+    true=db_user_remark.user_remark_booklist(data['booklist_id'],user_id,data['remark'])
+    remarks=db_user_remark.get_booklist_remark(data['booklist_id'],1,10)
 
-@mine_bp.route('/addtolist',methods=['POST','GET'])
-def addtolist():
+    return jsonify({'OK':true,'remarks':remarks})
+
+@mine_bp.route('/add_book_remark',methods=['POST','GET'])
+def add_book_remark():
     if request.method!='POST':
         return 'need POST'
     else:
         if flask_login.current_user.is_anonymous:
             return flask.redirect(flask.url_for('login_bp.login'))
         else:
-            account = flask_login.current_user.id
+            user_id = flask_login.current_user.id
 
-        bookname=request.form.get('bookname')
-        booklistname=request.form.get('booklistname')
+    data=json.loads(request.get_json())
+    true=db_user_remark.user_remark_book(data['book_id'],user_id,data['remark'])
+    remarks=db_user_remark.get_book_remark(data['book_id'],1,10)
+    return jsonify({'OK':true,'remarks':remarks})
 
-    return jsonify({bookname,booklistname})
+@mine_bp.route('/add_to_list',methods=['POST','GET'])
+def add_to_list():
+    if request.method!='POST':
+        return 'need POST'
+    else:
+        if flask_login.current_user.is_anonymous:
+            return flask.redirect(flask.url_for('login_bp.login'))
+        else:
+            user_id = flask_login.current_user.id
+
+        data=json.loads(request.get_json())
+        '''
+        add to list
+        
+        '''
+        true = True
+    return jsonify({'OK',true})
+
+@mine_bp.route('/vate_book',methods=['POST','GET'])
+def vate_book():
+    if request.method!='POST':
+        return 'need POST'
+    else:
+        if flask_login.current_user.is_anonymous:
+            return flask.redirect(flask.url_for('login_bp.login'))
+        else:
+            user_id = flask_login.current_user.id
+
+    data=json.loads(request.get_json())
+    current_vate=db_user_remark.get_book_vote(data['book_id'],user_id,data['attitude'])
+    if data['attitude']==current_vate:
+        '''
+        change attitude to neutral
+        '''
+    else:
+        '''
+        change attitude to data['attitude']
+        '''
+    jsondata={
+        'OK':True,
+        'attitude':'attitude'
+    }
+    return jsonify(jsondata)
+
+@mine_bp.route('/vote_booklist',methods=['POST','GET'])
+def vote_booklist():
+    return 'vote_booklist'
