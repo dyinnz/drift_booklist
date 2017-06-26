80fa9ab5f9e92bbbaea119589dda30051ff74036
Mein-Fuhrer <fuhrer.zhou@gmail.com>
2017-06-26 16:29:47 +0800

    Add simple explore page

---
+++ drift_app/exploer_page.py
@@ -0,0 +1,13 @@
+import logging
+import flask
+import flask_login
+from flask_login import current_user
+from flask import Blueprint, current_app, jsonify, request
+from drift_app.db_interface import db_user
+
+explore_bp = Blueprint('explore_bp', __name__)
+
+
+@explore_bp.route('/explore')
+def friends():
+    return current_app.send_static_file('react/explore.html')
