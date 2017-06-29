358fd2f9bef0e5ed9a558712033007c9c38fbb53
lpq_user <464798734@qq.com>
2017-06-29 15:40:05 +0800

    lpq9

--- react_app/index/initialize.js
+++ react_app/index/initialize.js
@@ -1,9 +1,15 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 
+import RecommandList from 'index/components/RecommandList';
+
+const App = () => (
+    <RecommandList/>
+);
+
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        <h1> Init Page </h1>,
+        <App/>,
         document.getElementById('root')
     );
 });
