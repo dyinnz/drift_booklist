0811d26026dcff79ccdc364b62778e4487c11565
lpq_user <464798734@qq.com>
2017-06-30 11:06:38 +0800

    lpq10

--- react_app/index/initialize.js
+++ react_app/index/initialize.js
@@ -2,14 +2,24 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 
 import RecommandList from 'index/components/RecommandList';
+import CategoryList from 'index/components/CategoryList';
 
 const App = () => (
     <RecommandList/>
+);
+
+const App2 = () => (
+    <CategoryList/>
 );
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
         <App/>,
         document.getElementById('root')
+    );
+
+    ReactDOM.render(
+        <App2/>,
+        document.getElementById('root2')
     );
 });
