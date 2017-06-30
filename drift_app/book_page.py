ae747640c9acd59290dc16058ea967a8f7045a32
dyinnz <dyinnz@gmail.com>
2017-06-30 09:27:08 +0800

    complete book page

--- drift_app/book_page.py
+++ drift_app/book_page.py
@@ -8,7 +8,7 @@ from drift_app.db_interface import db_user
 book_bp = Blueprint('book_bp', __name__)
 
 
-@book_bp.route('/book')
+@book_bp.route('/book/<book_id>')
 @flask_login.login_required
-def book():
+def book(book_id):
     return current_app.send_static_file('react/book.html')
--- react_app/book/components/BookPage.jsx
+++ react_app/book/components/BookPage.jsx
@@ -22,10 +22,6 @@ import ContentAdd from 'material-ui/svg-icons/content/add';
 
 import update from 'immutability-helper'
 
-import CommentPane from 'mine/components/CommentPane'
-import BooklistPane from 'mine/components/BooklistPane'
-
-
 function fetchPostJson(url, data) {
     console.log("fetchPostJson: ", data);
     return fetch(url, {
@@ -38,17 +34,277 @@ function fetchPostJson(url, data) {
     })
 }
 
+class Comment extends React.Component {
+    // TODO:
+    render() {
+        return (
+            <div className="flex">
+                <CardHeader title={this.props.details.account}
+                            avatar="/static/react/zen.jpg"
+                            className="comment_header"
+                />
+                <CardText>
+                    {this.props.details.remark}
+                </CardText>
+            </div>
+        )
+    }
+}
+
+class CommentList extends React.Component {
+    render() {
+        if (0 === this.props.items.length) {
+            return (
+                <div className="no_comment">
+                    <p>No comment here yet</p>
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    {this.props.items.map((item) => {
+                        return <Comment key={item.remark_time} details={item}/>
+                    })}
+                </div>
+            )
+        }
+    }
+}
+
+const labelStyle = {
+    color: blue500
+};
+
+class CommentBox extends React.Component {
+    handleCancel() {
+        let commentBox = document.getElementById("comment_box");
+        commentBox.value = ""
+    }
+
+    render() {
+        return (
+            <div className="reply_wrapper">
+                <TextField
+                    hintText="Add new comment here"
+                    multiLine={true}
+                    rows={1}
+                    rowsMax={3}
+                    fullWidth={true}
+                    id="comment_box"
+                />
+                <FlatButton
+                    label="reply"
+                    primary={true}
+                    onClick={this.props.handleReply}
+                />
+                <FlatButton
+                    label="cancel"
+                    secondary={true}
+                    onClick={() => this.handleCancel()}
+                />
+            </div>
+        )
+    }
+}
+
+class BookComment extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: props.items,
+            result: ""
+        }
+    }
+
+    componentWillReceiveProps(next) {
+        this.setState(update(this.state, {
+            items: {$set: next.items}
+        }))
+    }
+
+    handleReply() {
+        let commentBox = document.getElementById("comment_box");
+        if ('' === commentBox.value) {
+            this.setState(update(this.state, {
+                result: {$set: "Empty content!"}
+            }));
+            return
+        }
+
+        fetchPostJson("/add_book_remark", {
+            book_id: this.props.currBookID,
+            remark: commentBox.value
+        }).then(
+            resp => resp.json()
+        ).then((data) => {
+            if (data['OK']) {
+                this.setState({
+                    result: "Reply success",
+                    items: data.remarks
+                });
+                commentBox.value = ""
+            } else {
+                this.setState({
+                    result: "Reply Failed",
+                    items: this.state.items
+                })
+            }
+        });
+    }
+
+    renderHeader() {
+        return (
+            <div>
+                <div className="flex">
+                <Subheader> COMMENTS </Subheader>
+                <FlatButton
+                    label="NEW"
+                    primary={true}
+                    onClick={() => document.getElementById("comment_box").focus()}
+                />
+                </div>
+            </div>
+        )
+    }
+
+    renderBody() {
+        return (
+            <div>
+                <CommentList items={this.state.items}/>
+            </div>
+        )
+    }
+
+    renderCommentList() {
+        if (0 === this.props.items.length) {
+            return (
+                <div className="no_comment">
+                    <p>No comments here</p>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <CommentList items={this.state.items}/>
+            </div>
+        )
+    }
+
+    render() {
+        return (
+            <Paper>
+                {this.renderHeader()}
+                {this.renderCommentList()}
+                <CommentBox handleReply={this.handleReply.bind(this)}/>
+                <div className="comment_result">
+                    <p>{this.state.result}</p>
+                </div>
+            </Paper>
+        )
+    }
+}
+
 class BookDetails extends React.Component {
     render() {
-        <Card>
+        const styleBookCover = {
+            width: 300,
+            height: 400,
+        };
 
-        </Card>
+        const styleTagWrapper = {
+            marginLeft: 20,
+        };
+
+        let details = this.props.details;
+
+        if ("undefined" === typeof(details)) {
+            return <p>Waiting</p>
+        }
+
+        return (
+            <div>
+                <Card>
+                    <Subheader> BOOK DETAILS </Subheader>
+
+                    <div className="book_card">
+                        <CardMedia className="card_media">
+                            <img src="/static/react/default.png"
+                                 style={styleBookCover}
+                            />
+                        </CardMedia>
+
+                        <div>
+                            <CardHeader
+                                title={details.book_name}
+                                subtitle={details.publisher + "  " + details.ISBN}
+                            />
+
+                            <CardText>{details.introduction}</CardText>
+
+                            <Badge badgeContent={details.up_number}> <IconButton tooltip="Up">
+                                <ActionThumbUp/>
+                            </IconButton> </Badge>
+
+                            <Badge badgeContent={details.down_number}> <IconButton tooltip="Down">
+                                <ActionThumbDown/>
+                            </IconButton> </Badge>
+
+                            <Badge badgeContent={details.follower_number}> <IconButton tooltip="Star">
+                                <ActionStars/>
+                            </IconButton></Badge>
+
+                            <div style={styleTagWrapper}>
+                                <Chip>tag1</Chip>
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        )
     }
 }
 
 class BookPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            details: undefined,
+            comments: [],
+        };
+
+        let url = window.location.href;
+        this.book_id = url.substr(url.lastIndexOf('/') + 1);
+        console.log("book_id: ", this.book_id)
+    }
+
+    componentWillMount() {
+        fetchPostJson('/book_detail', {
+            book_id: this.book_id
+
+        }).then(
+            resp => resp.json()
+
+        ).then((data) => {
+            console.log(data);
+            this.setState(update(this.state, {
+                details: {$set: data},
+                comments: {$set: data.remarks},
+            }))
+        })
+    }
+
     render() {
-        return <BookDetails/>
+        return (
+            <div className="book_page">
+                <BookDetails details={this.state.details}/>
+                <BookComment items={this.state.comments}
+                             currBookID={this.book_id}
+                />
+            </div>
+        )
     }
 }
 
