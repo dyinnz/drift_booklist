2e4c3eac28a7ce62fb6e7d2a153d59d18f594d4b
Mein-Fuhrer <fuhrer.zhou@gmail.com>
2017-07-05 10:26:37 +0800

    ...

--- drift_app/db_interface/db_user_remark.py
+++ drift_app/db_interface/db_user_remark.py
@@ -914,3 +914,24 @@ def get_recommend_booklists(user_id, K=10):
     except Exception as e:
         logging.error(e)
         return None
+
+
+
+def get_recommend_booklist_by_tags(user_id, K=10):
+    try:
+        user = DB_user.query.filter_by(id=user_id).one()
+        interests = user.interests
+        d = {}
+        candidates = []
+        for tag in interests:
+            for bl in tag.booklists:
+                candidates.append(bl.id)
+                up_num = DB_user_booklist_opinion.query.filter_by(booklist_id=bl.id, vote='up').count()
+                d[bl.id] = get_booklist_follower_num(bl.id) + up_num
+        return sorted(candidates, key=lambda x: d[x], reverse=True)[:K]
+    except Exception as e:
+        logging.error("error in get_recommend_booklist_by_tags()")
+        logging.error(user_id)
+        logging.error(e)
+        return None
+
